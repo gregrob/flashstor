0de4f00829757f035cb88be3d4182a149484fb9f
Greg Robinson <gregrob@mac.com>
2023-09-14 21:47:44 +1000

    scripts to start / stop all symlinked containers

---
+++ servers/containers-start.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Shortcut to the server's folder
+SERVER_PATH=./${HOSTNAME}
+
+# Docker method (start or stop)
+DOCKER_METHOD="docker-start.sh"
+DOCKER_METHOD_STR="Starting"
+
+# Change into the servers folder
+cd ${SERVER_PATH}
+
+# Loop through all of the folders
+for dir in */; do
+
+    # Change into container folder
+    cd $dir
+
+    echo "${DOCKER_METHOD_STR} container inside $dir"
+    eval ../$DOCKER_METHOD v2
+
+    # Change out of container
+    cd ..
+
+done
+
